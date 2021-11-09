6f6134caf07b25978e1bca7ee038368a97bcb212
jiy12345 <jiy12367gmail.com>
2021-11-09 22:15:29 +0900

    rest_framework 사용을 위한 코드 추가

--- Server/config/settings.py
+++ Server/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # rest framework 사용 위해 추가
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Server/config/urls.py
+++ Server/config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # rest_framework
+    # url path는 변경하여 사용 가능
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
