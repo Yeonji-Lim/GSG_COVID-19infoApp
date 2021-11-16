5c96171d9a66f0b7db1349d156c825098563d59a
sidereumare <48210134+sidereumare@users.noreply.github.com>
2021-11-16 18:56:45 +0900

    데이터 클래스 생성, build.gradle수정

--- COVID_19info/app/src/main/java/com/example/covid_19info/data/routeAPI.kt
+++ COVID_19info/app/src/main/java/com/example/covid_19info/data/routeAPI.kt
@@ -1,6 +1,11 @@
 package com.example.covid_19info.data
 
+import android.util.Log
+import okhttp3.Call
 import org.json.JSONObject
+import retrofit2.http.GET
+import com.example.covid_19info.data.model.Quarantine
+
 import java.net.URLDecoder
 import java.net.URLEncoder
 import java.net.URL
@@ -8,31 +13,42 @@ import java.net.HttpURLConnection
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-object routeAPI {
-    var data = JSONObject()
+//object routeAPI {
+//    var data = JSONObject()
+//
+//    init{
+//        var client = DefaultHttpClient()
+//
+//
+//        var url = URL("https://coroname.me/getdata")
+//        //var url = URL("")
+//        var conn = url.openConnection() as HttpURLConnection
+//        conn.requestMethod = "GET"
+//        conn.setRequestProperty("Content-type", "application/json")
+//        var rd:BufferedReader
+//        Log.d("main2", "${conn.responseCode}")
+//        rd = if(conn.responseCode in 200..300) {
+//            BufferedReader(InputStreamReader(conn.inputStream))
+//        } else {
+//            BufferedReader(InputStreamReader(conn.errorStream))
+//        }
+//
+//        var sb = StringBuilder()
+//        var line = rd.readLine()
+//        while (line != null) {
+//            sb.append(line)
+//            line = rd.readLine()
+//        }
+//        rd.close()
+//        conn.disconnect()
+//
+//        data = JSONObject(sb.toString().trimIndent())
+//    }
+//}
 
-    init{
-        //var url = URL("https://coroname.me/getdata")
-        var url = URL("")
-        var conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "GET"
-        conn.setRequestProperty("Content-type", "application/json")
-        var rd:BufferedReader
-        rd = if(conn.responseCode in 200..300) {
-            BufferedReader(InputStreamReader(conn.inputStream))
-        } else {
-            BufferedReader(InputStreamReader(conn.errorStream))
-        }
-
-        var sb = StringBuilder()
-        var line = rd.readLine()
-        while (line != null) {
-            sb.append(line)
-            line = rd.readLine()
-        }
-        rd.close()
-        conn.disconnect()
-
-        data = JSONObject(sb.toString().trimIndent())
-    }
+interface routeAPI{
+//    @GET("getdata")
+//    fun getData(
+//
+//    ): Call<Quarantine>
 }
