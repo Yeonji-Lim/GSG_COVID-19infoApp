3f08a630c89cb22771f791dde4fe3e667a2d0506
dolppe <35285591+dolppe@users.noreply.github.com>
2021-11-16 20:36:15 +0900

    테스팅 코드

--- COVID_19info/app/src/test/java/com/example/covid_19info/ExampleUnitTest.kt
+++ COVID_19info/app/src/test/java/com/example/covid_19info/ExampleUnitTest.kt
@@ -4,6 +4,15 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+import com.example.covid_19info.data.routeAPI
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,6 +21,41 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        //var i = routeAPI.data.keys();
+
+       // while(i.hasNext())
+        {
+           // var a = i.next().toString();
+            //print(a);
+        }
+
+        var url = URL("https://coroname.me/getdata")
+        //var url = URL("")
+        var conn = url.openConnection() as HttpURLConnection
+        conn.requestMethod = "GET"
+        conn.setRequestProperty("Content-type", "application/json")
+        var rd: BufferedReader
+        rd = if(conn.responseCode in 200..300) {
+            BufferedReader(InputStreamReader(conn.inputStream))
+        } else {
+            BufferedReader(InputStreamReader(conn.errorStream))
+        }
+
+        var sb = StringBuilder()
+        var line = rd.readLine()
+        while (line != null) {
+            sb.append(line)
+            line = rd.readLine()
+        }
+        rd.close()
+        conn.disconnect()
+
+        var data = JSONObject(sb.toString().trimIndent()).getJSONObject("data")
+
+
+        var temp = data.getString("region")
+        print("$temp")
+
+
     }
 }
