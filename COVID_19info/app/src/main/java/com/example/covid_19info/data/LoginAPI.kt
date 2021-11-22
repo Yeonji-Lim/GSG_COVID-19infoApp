fe67e5c93e96ecab006b04e38131d8f0063d05ce
sidereumare <48210134+sidereumare@users.noreply.github.com>
2021-11-22 23:17:46 +0900

    loginapi

---
+++ COVID_19info/app/src/main/java/com/example/covid_19info/data/LoginAPI.kt
@@ -0,0 +1,39 @@
+package com.example.covid_19info.data
+
+
+import android.util.Log
+import com.example.covid_19info.data.model.LoginToken
+import com.example.covid_19info.data.model.SignUpRst
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+
+interface LoginAPI {
+
+    @Headers("Content-Type: application/json")
+    @POST("signup/")
+    fun signup(
+    @Body signUpRst: SignUpRst
+    ): Call<SignUpRst>
+
+    @POST("login/")
+    fun login(
+
+    ): Call<LoginToken>
+
+    companion object{
+        private const val BASE_URL_ROUTE="https://dcec53b3-9e80-4db3-954c-9605d320a60d.mock.pstmn.io/"
+        fun create(): LoginAPI{
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL_ROUTE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(LoginAPI::class.java)
+        }
+
+    }
+}
--- COVID_19info/app/src/test/java/com/example/covid_19info/ExampleUnitTest.kt
+++ COVID_19info/app/src/test/java/com/example/covid_19info/ExampleUnitTest.kt
@@ -12,7 +12,13 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.*
-
+import com.example.covid_19info.data.LoginAPI
+import com.example.covid_19info.data.model.LoginToken
+import com.example.covid_19info.data.model.Quarantines
+import com.example.covid_19info.data.model.SignUpRst
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -20,49 +26,72 @@ import java.util.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        //var i = routeAPI.data.keys();
+//
+//       // while(i.hasNext())
+//        {
+//           // var a = i.next().toString();
+//            //print(a);
+//        }
+//
+//        var url = URL("https://coroname.me/getdata")
+//        //var url = URL("")
+//        var conn = url.openConnection() as HttpURLConnection
+//        conn.requestMethod = "GET"
+//        conn.setRequestProperty("Content-type", "application/json")
+//        var rd: BufferedReader
+//        rd = if(conn.responseCode in 200..300) {
+//            BufferedReader(InputStreamReader(conn.inputStream))
+//        } else {
+//            BufferedReader(InputStreamReader(conn.errorStream))
+//        }
+//
+//        var sb = StringBuilder()
+//        var line = rd.readLine()
+//        while (line != null) {
+//            sb.append(line)
+//            line = rd.readLine()
+//        }
+//        rd.close()
+//        conn.disconnect()
+//
+//        var data = JSONObject(sb.toString().trimIndent()).getJSONObject("data")
+//
+//
+//        var temp = data.getString("region")
+//        print("$temp")
+//
+//
+//    }
+//    @Test
+//    fun addition_isCorrect() {
+//        //var q = QuarantinesRouteAPI
+//        var data = QuarantinesRouteAPI.data
+//        data.data[0]
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        //var i = routeAPI.data.keys();
-
-       // while(i.hasNext())
-        {
-           // var a = i.next().toString();
-            //print(a);
-        }
-
-        var url = URL("https://coroname.me/getdata")
-        //var url = URL("")
-        var conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "GET"
-        conn.setRequestProperty("Content-type", "application/json")
-        var rd: BufferedReader
-        rd = if(conn.responseCode in 200..300) {
-            BufferedReader(InputStreamReader(conn.inputStream))
-        } else {
-            BufferedReader(InputStreamReader(conn.errorStream))
-        }
-
-        var sb = StringBuilder()
-        var line = rd.readLine()
-        while (line != null) {
-            sb.append(line)
-            line = rd.readLine()
-        }
-        rd.close()
-        conn.disconnect()
-
-        var data = JSONObject(sb.toString().trimIndent()).getJSONObject("data")
-
-
-        var temp = data.getString("region")
-        print("$temp")
-
-
-    }
-    @Test
-    fun addition_isCorrect() {
-        //var q = QuarantinesRouteAPI
-        var data = QuarantinesRouteAPI.data
-        data.data[0]
+    fun test_isCor(){
+        var q = LoginAPI.create()
+        var d = SignUpRst(email = "123@123", first_name = "김", last_name = "병현", password = "123")
+        var d1 = LoginToken(token=null)
+        //q.signup(d).execute()
+//        q.signup(d).enqueue(object: Callback<SignUpRst> {
+//
+//            override fun onResponse(call: Call<SignUpRst>,
+//                                    response: Response<SignUpRst>
+//            ) {
+//                Log.d("Main", "성공 : ${response.raw()}")
+//            }
+//            override fun onFailure(call: Call<SignUpRst>, t: Throwable) {
+//                Log.d("Main", "실패 : ${t}")
+//                var m = t.message
+//                print(m)
+//            }
+//        })
+        print(q.signup(d).execute().body())
+        print(q.login().execute().body())
     }
 }
