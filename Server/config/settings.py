2c6639a4ebf33f7cb841b56c87632d11f2454325
jiy12345 <jiy12367gmail.com>
2021-11-24 15:32:52 +0900

    이메일 발신자 관련 설정을 위한 settings.py 수정

--- Server/config/settings.py
+++ Server/config/settings.py
@@ -137,3 +137,42 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+BaseDIR = os.path.dirname(os.path.abspath(__file__))
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+# 이메일의 발신자 정보 설정
+EMAIL_FROM = os.environ.get('AUTHEMAIL_DEFAULT_EMAIL_FROM')
+EMAIL_BCC = os.environ.get('AUTHEMAIL_DEFAULT_EMAIL_BCC')
+
+# 메일을 호스트할 서버
+EMAIL_HOST = 'smtp.gmail.com'
+
+# Gmail과 통신할 포트
+EMAIL_PORT = '587'
+
+# 발신할 이메일 주소
+EMAIL_HOST_USER = get_secret("EMAIL_HOST_USER")
+
+# 발신할 이메일의 비밀번호
+EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
+
+# TLS 보안 방법 설정
+EMAIL_USE_TLS = True
+
+# 사이트와 관련한 자동응답을 받을 이메일 주소
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
