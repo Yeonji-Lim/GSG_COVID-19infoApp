e1060c200b2131a692eb0004f0fc0738baf2bdcd
jiy12345 <jiy12367gmail.com>
2021-11-24 15:25:32 +0900

    authemail의 기능 활용을 위한 url 작성

--- Server/config/urls.py
+++ Server/config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # authemail 사용을 위한 url 작성
+    path('api/accounts/', include('authemail.urls')),
 ]
