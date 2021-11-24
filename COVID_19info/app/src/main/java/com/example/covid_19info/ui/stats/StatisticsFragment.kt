b389302e45cf6ef936460c054f71ee4a01d35c5b
sidereumare <48210134+sidereumare@users.noreply.github.com>
2021-11-24 23:53:24 +0900

    gradle 문제 해결

--- COVID_19info/app/src/main/java/com/example/covid_19info/ui/stats/StatisticsFragment.kt
+++ COVID_19info/app/src/main/java/com/example/covid_19info/ui/stats/StatisticsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.covid_19info.R
+import com.example.covid_19info.databinding.StatisticsFragmentBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,12 +19,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StatisticsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+
+    private lateinit var binding: StatisticsFragmentBinding
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //바인딩 연결
+        binding = StatisticsFragmentBinding.inflate(layoutInflater)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,7 +41,7 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false)
+        return inflater.inflate(R.layout.statistics_fragment, container, false)
     }
 
     companion object {
