c27aede7ed012d017355c5fb5707a713777519c2
Yeonji-Lim <view7186@naver.com>
2021-11-27 10:53:47 +0900

    ALLOWED_HOSTS 수정

--- Server/config/settings.py
+++ Server/config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
